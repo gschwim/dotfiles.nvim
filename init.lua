b76b9cd3718a923bc90e83c3e29ed43ec6ccbb8d
Greg Schwimer <gschwim@users.noreply.github.com>
2023-07-24 16:57:09 -0700

    Update init.lua
    Per fidget gh page, use legacy tag so nvim stops complaining about the thing being upgraded in the future at nvim start

--- init.lua
+++ init.lua
@@ -84,7 +84,9 @@ require('lazy').setup({
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
+      -- 07.24.2023 - per fidget gh page, use legacy tag so nvim stops complaining
+      -- about the thing being upgraded in the future at nvim start
+      { 'j-hui/fidget.nvim', tag='legacy', opts = {} },
 
       -- Additional lua configuration, makes nvim stuff amazing!
       'folke/neodev.nvim',
