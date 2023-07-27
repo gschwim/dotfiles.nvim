4f9be825bd665d87e3918e99f1c75a2573488d8f
Greg Schwimer <schwim@bitrail.com>
2023-07-26 17:58:44 -0700

    moved plugins up to lua/plugins. First test using opts in treesitter plugin to centralize plugin config to its own file.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,7 +2,10 @@
 
 return {
   -- Fuzzy Finder (files, lsp, etc)
-  { 'nvim-telescope/telescope.nvim', version = '*', dependencies = { 'nvim-lua/plenary.nvim' } },
+  { 'nvim-telescope/telescope.nvim',
+    version = '*',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
 
   -- Fuzzy Finder Algorithm which requires local dependencies to be built.
   -- Only load if `make` is available. Make sure you have the system
