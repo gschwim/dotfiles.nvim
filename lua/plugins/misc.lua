a322d61e0e147b7926c96016fffea239b0c0a7f8
Greg Schwimer <schwim@bitrail.com>
2024-05-12 16:44:20 -0700

    indent-blankline to own file

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -19,19 +19,6 @@ return {
     dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
   },
 
-  { -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
-    event = { "BufReadPre", "BufNewFile" },
-    main = "ibl",
-    opts = {
-      -- indent = { highlight = { "CursorColumn", "Whitespace" }, char = "▏" },
-      indent = { char = '┊'},
-      -- whitespace = { highlight = { "Whitespace", "NonText" } },
-      -- show_trailing_blankline_indent = false,
-    },
-  },
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
