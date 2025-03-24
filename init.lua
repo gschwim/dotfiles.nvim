4f95ce5322673949d1317795ccff156d5b107132
Greg Schwimer <schwim@bitrail.com>
2025-03-24 10:23:19 -0700

    flutter tools tuning, moved lsp on_attach fn to lsp_utils module

--- init.lua
+++ init.lua
@@ -1,40 +1,14 @@
 --[[
 
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-
-Kickstart.nvim is *not* a distribution.
-
-Kickstart.nvim is a template for your own configuration.
-  The goal is that you can read every line of code, top-to-bottom, and understand
-  what your configuration is doing.
-
-  Once you've done that, you should start exploring, configuring and tinkering to
-  explore Neovim!
 
   If you don't know anything about Lua, I recommend taking some time to read through
   a guide. One possible example:
   - https://learnxinyminutes.com/docs/lua/
 
-  And then you can explore or search through `:help lua-guide`
-
-
-Kickstart Guide:
-
-I have left several `:help X` comments throughout the init.lua
-You should run that command and read that help section for more information.
-
-In addition, I have some `NOTE:` items throughout the file.
-These are for you, the reader to help understand what is happening. Feel free to delete
-them once you know what you're doing, but they should serve as a guide for when you
-are first encountering a few different constructs in your nvim config.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now :)
 --]]
+
+-- Imports go here
+local lsp_utils = require('lsp_utils')
 
 -- Set <space> as the leader key
 -- See `:help mapleader`
@@ -205,51 +179,6 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
--- LSP settings.
---  This function gets run when an LSP connects to a particular buffer.
-local on_attach = function(_, bufnr)
-  -- NOTE: Remember that lua is a real programming language, and as such it is possible
-  -- to define small helper and utility functions so you don't have to repeat yourself
-  -- many times.
-  --
-  -- In this case, we create a function that lets us more easily define mappings specific
-  -- for LSP related items. It sets the mode, buffer and description for us each time.
-  local nmap = function(keys, func, desc)
-    if desc then
-      desc = 'LSP: ' .. desc
-    end
-
-    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-  end
-
-  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-  nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-  nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-  -- See `:help K` for why this keymap
-  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
-  -- Lesser used LSP functionality
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  nmap('<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, '[W]orkspace [L]ist Folders')
-
-  -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-    vim.lsp.buf.format()
-  end, { desc = 'Format current buffer with LSP' })
-end
-
 -- Enable the following language servers
 --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
 --
@@ -291,7 +220,7 @@ mason_lspconfig.setup_handlers {
   function(server_name)
     require('lspconfig')[server_name].setup {
       capabilities = capabilities,
-      on_attach = on_attach,
+      on_attach = lsp_utils.on_attach,
       settings = servers[server_name],
     }
   end,
--- lua/plugins/flutter-tools.lua
+++ lua/plugins/flutter-tools.lua
@@ -6,11 +6,33 @@ return {
         'stevearc/dressing.nvim', -- optional for vim.ui.select
     },
     config = function()
+          local lsp_utils = require('lsp_utils')
           require("flutter-tools").setup {
             -- Basic flutter-tools configuration
             lsp = {
               color = { enabled = true }, -- Optional: enable UI enhancements
-              -- on_attach = on_attach,
+              on_attach = function(client, bufnr)
+                  -- Call the shared on_attach from lsp_utils
+                  lsp_utils.on_attach(client, bufnr)
+
+                  -- Add more Flutter-specific keybindings
+                  vim.keymap.set("n", "<leader>f", "<cmd>Telescope flutter commands<cr>", {
+                    buffer = bufnr,
+                    desc = "Flutter Run",
+                  })
+                  vim.keymap.set("n", "<leader>fq", "<cmd>FlutterQuit<cr>", {
+                    buffer = bufnr,
+                    desc = "Flutter Quit",
+                  })
+                  vim.keymap.set("n", "<leader>fr", "<cmd>FlutterRun<cr>", {
+                    buffer = bufnr,
+                    desc = "Flutter Run",
+                  })
+                end,
+
+
+
+              capabilities = require('cmp_nvim_lsp').default_capabilities(),
             },
             -- Load the Telescope extension
             telescope = require("telescope").load_extension("flutter"),
