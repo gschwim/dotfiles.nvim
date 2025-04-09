1bf0ec3233b497e45686c5f4902997e5233d6ad3
Greg Schwimer <schwim@bitrail.com>
2025-04-08 22:10:51 -0700

    added svelte plugin, cleaned up flutter tools to lazy load

--- lua/plugins/flutter-tools.lua
+++ lua/plugins/flutter-tools.lua
@@ -1,6 +1,7 @@
 return {
     'nvim-flutter/flutter-tools.nvim',
-    lazy = false,
+    event = { "BufReadPre", "BufNewFile" },
+    -- lazy = false,
     dependencies = {
         'nvim-lua/plenary.nvim',
         'stevearc/dressing.nvim', -- optional for vim.ui.select
